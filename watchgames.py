8b1680b26f608bef73cbec9de279295f35e46384
Kevin <kevin.matthew.obrien@gmail.com>
2018-04-09 23:26:14 -0400

    Update watchgames.py

--- watchgames.py
+++ watchgames.py
@@ -1,44 +1,40 @@
-import requests
-import json
-import time
-import datetime
-import urllib.request
-import urllib
-from pprint import pprint
-
-team_name = "Washington Capitals" # Set this to your desired team (ex. Washington Capitals)
-NBCSP = '590' # Set this to your local tv channel number for this network
-NBCSWA = '576' # Set this to your local tv channel number for this network
-
-todaysdate = time.strftime("%Y-%m-%d") # Get today's date to limit
-
-api_url = ("https://statsapi.web.nhl.com/api/v1/schedule?startDate="+todaysdate+"&endDate="+todaysdate+"&expand=schedule.broadcasts.all")
-
-data = requests.get(api_url).json()
-
-for game in data['dates'][0]['games']:
-    if (game['teams']['away']['team']['name'] == team_name) or (game['teams']['home']['team']['name'] == team_name):
-        print(game['teams']['away']['team']['name'] + ' at ' + game['teams']['home']['team']['name'])
-        game_time = datetime.datetime.strptime(game['gameDate'], "%Y-%m-%dT%H:%M:%SZ") # convert string to timestamp
-        print(game_time)
-
-        # Determine correct TV channel
-        for broadcast in game['broadcasts']:
-            if broadcast['name'] == "NBCSP":
-                tv_channel = NBCSP
-            elif broadcast['name'] == "NBCSWA":
-                tv_channel = NBCSWA
-
-        print(tv_channel)
-
-current_status = urllib.request.urlopen("http://192.168.2.47:8282/hubs/harmony-hub/status").read().decode('UTF-8') # Get current status data
-current_status = json.loads(current_status)
-current_activity = current_status['current_activity'].items() # Get current_activity information from current_status
-
-is_off = current_status['off']
-
-# Check to see if harmony is off, if so, start activity
-if (is_off == True):
-    print(requests.post(url='http://192.168.2.47:8282/hubs/harmony-hub/activities/watchtv'))
-
-print(is_off)
+import requests                                                                                                               
+import json                                                                                                                   
+import time                                                                                                                   
+import datetime                                                                                                               
+import urllib                                                                                                                 
+import pyharmony                                                                                                              
+import os                                                                                                                     
+import logging                                                                                                                
+import pyharmony_client                                                                                                       
+                                                                                                                              
+def main():                                                                                                                   
+    default_activity = "watchtv" # Set to the slug for your Harmony activity to watch TV                                      
+    harmony_hub_ip = "192.168.2.235" # IP address for harmony hub device                                                      
+    harmony_port = "5222" # Port for harmony hub device default 5222                                                          
+                                                                                                                              
+    '''                                                                                                                       
+    if (is_off):                                                                                                              
+        print(watch_tv())                                                                                                     
+        time.sleep(5)                                                                                                         
+                                                                                                                              
+    while current_activity != default_activity: # perform loop while current activity is not set to default_activity          
+        print(watch_tv()) # turn on default activity                                                                          
+        time.sleep(5)                                                                                                         
+        is_off, current_status, current_activity = get_current_status() # refresh current_status                              
+        if current_activity == default_activity: # check if default_activity is active now and break loop                     
+            break                                                                                                             
+    '''                                                                                                                       
+                                                                                                                              
+    token = pyharmony_client.get_token(harmony_hub_ip, harmony_port)                                                          
+                                                                                                                              
+    config = pyharmony_client.get_config(token, harmony_hub_ip, harmony_port)                                                 
+                                                                                                                              
+    print(pyharmony_client.get_current_activity(token, config, harmony_hub_ip, harmony_port))                                 
+                                                                                                                              
+    print(pyharmony_client.start_activity(token, harmony_hub_ip, harmony_port, config, default_activity))                     
+                                                                                                                              
+    print(pyharmony_client.change_channel(token, harmony_hub_ip, harmony_port, '576'))                                        
+                                                                                                                              
+if __name__ == "__main__":                                                                                                    
+    main()                                                                                                                    
